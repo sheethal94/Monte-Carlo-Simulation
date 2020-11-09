563e9d905fa845633e2e7c98ee8f83b49b43f091
sheethal94 <74208394+sheethal94@users.noreply.github.com>
2020-11-09 17:30:51 -0500

    Create Confidence.java

---
+++ Confidence.java
@@ -0,0 +1,27 @@
+/*
+ * Confidence.java
+ *
+ * Version:
+ *     $1.0$
+ *
+ * Revisions:
+ *     $Log$
+ */
+
+/**
+ * This program initializes confidenceTree and consensusTree
+ *
+ *  
+ * @author      Sheethal Umesh Nagalakshmi
+ */
+public class Confidence {
+    public String confidenceTree;
+
+    public String consensusTree;
+
+    public Confidence(String confidenceTree, String consensusTree) {
+        this.confidenceTree = confidenceTree;
+        this.consensusTree = consensusTree;
+    }
+}
+
